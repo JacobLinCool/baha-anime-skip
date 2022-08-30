dc272001389799f5c195625b6ca49a5dcc2ac0a4
JacobLinCool <jacoblincool@gmail.com>
2022-08-31 00:51:00 +0800

    feat: complete poc

--- packages/baha-anime-skip/tsup.config.ts
+++ packages/baha-anime-skip/tsup.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from "tsup";
+import PACKAGE from "./package.json";
 
-export default defineConfig((options) => ({
+const banner = `// ==UserScript==
+// @name         Baha Anime Skip
+// @version      ${PACKAGE.version}
+// @description  ${PACKAGE.description}
+// @author       JacobLinCool <jacoblincool@gmail.com> (https://github.com/JacobLinCool)
+// @license      MIT
+// @homepage     https://github.com/JacobLinCool/baha-anime-skip#readme
+// @supportURL   https://github.com/JacobLinCool/baha-anime-skip/issues
+// @namespace    http://tampermonkey.net/
+// @match        https://ani.gamer.com.tw/animeVideo.php?sn=*
+// @icon         https://www.google.com/s2/favicons?domain=gamer.com.tw
+// @grant        none
+// ==/UserScript==
+`;
+
+export default defineConfig(() => ({
     entry: ["src/index.ts"],
-    outDir: "lib",
+    outDir: "dist",
     target: "node16",
     format: ["esm"],
     clean: true,
     splitting: false,
+    banner: () => ({
+        js: banner,
+    }),
 }));
