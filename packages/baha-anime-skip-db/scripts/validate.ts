9f3c1e5c870b51f69c6deadb1388ac1ff4887843
JacobLinCool <jacoblincool@gmail.com>
2022-09-02 02:45:15 +0800

    feat: db formater

--- packages/baha-anime-skip-db/scripts/validate.ts
+++ packages/baha-anime-skip-db/scripts/validate.ts
@@ -1,8 +1,14 @@
 import fs from "node:fs";
 import { z } from "zod";
+import prettier from "prettier";
 
 const schema = z.record(z.string().regex(/^\d+$/), z.record(z.tuple([z.number(), z.number()])));
 
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 
 schema.parse(data);
+
+fs.writeFileSync(
+    "data.json",
+    prettier.format(JSON.stringify(data), { parser: "json", tabWidth: 4 }),
+);
