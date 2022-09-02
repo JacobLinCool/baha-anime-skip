d394cb9fa19507bc0da8f3c473c361276127fac3
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-09-02 08:23:30 +0000

    deploy: 6245b8dfc15dc89358c924dd680f09de1a4d597c

--- index.user.js
+++ index.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Baha Anime Skip
-// @version      0.1.0
+// @version      0.1.1
 // @description  Skip OP or other things on Bahamut Anime.
 // @author       JacobLinCool <jacoblincool@gmail.com> (https://github.com/JacobLinCool)
 // @license      MIT
